9669e4d728aa1a3070436f1d3c09eba82b1ca408
1MioSubs <it09022022@gmail.com>
2023-08-19 21:11:20 +0300

    first commit 3

---
+++ folio/1/js/main.js
@@ -0,0 +1,45 @@
+const tabsItem = document.querySelectorAll(".tabs__btn-item");
+const tabsContent = document.querySelectorAll(".tabs__content-item");
+
+tabsItem.forEach(function (element) {
+  element.addEventListener("click", open);
+});
+
+function open(evt) {
+  const tabTarget = evt.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabsItem.forEach(function (item) {
+    item.classList.remove("tabs__btn-item--active");
+  });
+
+  tabTarget.classList.add("tabs__btn-item--active");
+
+  tabsContent.forEach(function (item) {
+    item.classList.remove("tabs__content-item--active");
+  });
+
+  document
+    .querySelector(`#${button}`)
+    .classList.add("tabs__content-item--active");
+}
+
+const menuBtn = document.querySelector(".menu__btn");
+const menu = document.querySelector(".menu__list");
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu__list--active');
+})
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
